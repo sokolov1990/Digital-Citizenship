98b2db7bee42f03848e3b6d519e5103643bb47bc
sokolov1990 <82565779+sokolov1990@users.noreply.github.com>
2021-09-16 21:55:54 +0300

    Update level7.js

--- js/level7.js
+++ js/level7.js
@@ -1,11 +1,13 @@
 function yes() {
     document.querySelector('.container').style.display = 'none';
+    document.querySelector('.container1').style.display = 'none';
     document.querySelector('.otvet1').style.display = 'block';
 
 }
 
 function no() {
     document.querySelector('.container').style.display = 'none';
+    document.querySelector('.container1').style.display = 'none';
     document.querySelector('.otvet2').style.display = 'block';
 
 }
