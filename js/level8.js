f00e92ae466aa5a54ea4a0fd570a9a6312938304
sokolov1990 <82565779+sokolov1990@users.noreply.github.com>
2021-09-16 12:19:16 +0300

    Add files via upload

---
+++ js/level8.js
@@ -0,0 +1,12 @@
+function search() {
+    if (document.querySelector('input').value == 'пирог с ягодами' ||
+        document.querySelector('input').value == 'рецепт пирога с ягодами' ||
+        document.querySelector('input').value == 'Пирог с ягодами' ||
+        document.querySelector('input').value == 'Рецепт пирога с ягодами') {
+        document.querySelector('.container').style.display = 'none';
+        document.querySelector('.container1').style.display = 'block';
+    } else {
+        document.querySelector('.container').style.display = 'none';
+        document.querySelector('.container2').style.display = 'block';
+    }
+}
